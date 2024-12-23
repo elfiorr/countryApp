1f1d75ec76294068e7e7bc00f7143dbaf138bb83
elfiorr <elfiorr@gmail.com>
2024-12-23 07:49:19 +0100

    second commit

---
+++ src/main/java/com/geo/country/EndpointsFactory.java
@@ -0,0 +1,22 @@
+package com.geo.country;
+
+public class EndpointsFactory {
+
+    public static final String API = "/api";
+    public static final String COUNTRIES = "/countries";
+    public static final String CONTINENTS = "/continents";
+    public static final String CONTACT = "/contact";
+    public static final String ALL = "/all";
+    public static final String ID = "/{id}";
+    public static final String GET_BY_NAME_OF_COUNTRY = "/nameOfCountry";
+    public static final String GET_BY_ID_OF_COUNTRY = "/countryId";
+    public static final String GET_BY_NAME_OF_CONTINENT = "/nameOfContinent";
+    public static final String GET_BY_CAPITAL = "/capital";
+    public static final String EDIT = "/edit/{id}";
+    public static final String DELETE = "/delete/{id}";
+    public static final String ADD_NEW = "/addNew";
+    public static final String CITIES = "/cities";
+    public static final String GET_BY_CITY = "/city";
+    public static final String GET_BY_CITY_WITH_WEATHER = "/cityWithWeather";
+    public static final String GET_WEATHER = "/weather";
+}
